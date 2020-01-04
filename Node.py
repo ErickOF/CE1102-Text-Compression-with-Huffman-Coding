a080956ca1a5ecbc9d6fa1e4b654fe60075f3019
Erick Andrés Obregón Fonseca <erickobregonf@gmail.com>
2020-01-04 09:29:29 -0600

    Node and Tree classes were modified

--- Node.py
+++ Node.py
@@ -1,37 +1,55 @@
 class Node:
-    def __init__ (self, token, repetions):
+    def __init__ (self, token, value):
         self.__left = None
         self.__right = None
         self.__token = token
-        self.__repetions = repetions
+        self.__value = value
+
+    def getLeft(self):
+        return self.__left
+
+    def getRight(self):
+        return self.__right
 
     def getToken(self):
         return self.__token
 
-    def getRepetions(self):
-        return self.__repetions
+    def getValue(self):
+        return self.__value
+
+    def setLeft(self, left):
+        self.__left = left
+
+    def setRight(self, right):
+        self.__right = right
 
     def setToken(self, token):
         self.__token = token
 
-    def setRepetions(self, repetions):
-        self.__repetions = repetions
-
-    def isLeaf(self):
-        return self.__lef != None
-
-    def insert(self, node):
-        if self.__left == None:
-            self.__left = node
-        elif self.__right == None:
-            self.__right = node
+    def setValue(self, value):
+        self.__value = value
 
     def openXMLTag(self):
-        return "<" + self.__token + "-" + str(self.__repetions) + ">"
+        return "<" + self.__token + "-" + str(self.__value) + ">"
 
     def closeXMLTag(self):
-        return "</" + self.__token + "-" + str(self.__repetions) + ">"
+        return "</" + self.__token + "-" + str(self.__value) + ">"
 
-    def printNodesList(nodes):
-        for node in nodes:
-            print(node.getToken() + " " + str(node.getRepetions()))
+    def __str__(self):
+        return node.getToken() + " " + str(node.getValue())
+
+    def insert(self, token, value):
+        if self.__value:
+            if value < self.__value:
+                if self.__left:
+                    self.__left.insert(token, value)
+                else:
+                    self.__left = Node(token, value)
+            elif value >= self.__value:
+                if self.__right:
+                    self.__right.insert(token, value)
+                else:
+                    self.__right = Node(token, value)
+        else:
+            self.__value = value
+            self.__toke = token
--- Tree.py
+++ Tree.py
@@ -1,6 +1,35 @@
-class Tree():
-    def __init__ (self, node):
-        self.root = node
+from node import *
 
+
+class Tree():
+    def __init__ (self, root=None):
+        self.__root = root
+
+    def insert(self, token, value):
+        if self.__root:
+            self.__root.insert(token, value)
+        else:
+            self.__root = Node(token, value)
+
+    def __showTree(self, node):
+        if node:
+            return node.openXMLTag() + self.__showTree(node.getLeft()) + self.__showTree(node.getRight()) + node.closeXMLTag()
+        return ""
+            
     def __str__(self):
-        return self.token + str(self.numRepeticiones)
+        if self.__root == None:
+            return "<>"
+        return self.__showTree(self.__root)
+    
+
+if __name__ == '__main__':
+    tree = Tree()
+    print(tree)
+    tree.insert('a', 100)
+    tree.insert('b', 50)
+    tree.insert('e', 300)
+    tree.insert('c', 25)
+    tree.insert('d', 75)
+    tree.insert('f', 200)
+    tree.insert('g', 400)
+    print(tree)
